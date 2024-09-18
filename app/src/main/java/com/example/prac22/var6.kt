daa7d8c301e2e91a9be235837d7a884cf4e278c5
ural-ECTS <ural@ects.ru>
2024-09-18 11:51:55 +0500

    first commit

---
+++ app/src/main/java/com/example/prac22/var6.kt
@@ -0,0 +1,16 @@
+package com.example.prac22
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class var6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_var6)
+    }
+
+    fun search(view: View){
+
+    }
+}
